7a465c2aa107c31e66032c0a89aee87646553315
Yonsy Solis <yonsy.solis@outlook.com>
2023-02-13 00:29:52 -0500

    rbac roles

--- rbac.tf
+++ rbac.tf
@@ -14,3 +14,39 @@ resource "aws_iam_policy" "eks_admin_policy" {
     ]
   })
 }
+
+module "front-engineer-role" {
+  source  = "tf-mod/rbac-role/aws"
+  version = "1.0.0"
+
+  name        = "front-engineer-role"
+  stack       = "dev-qa-prod"
+  policy_arn  = ["arn:aws:iam::aws:policy/AmazonRedshiftFullAccess"]
+}
+
+module "back-engineer-role" {
+  source  = "tf-mod/rbac-role/aws"
+  version = "1.0.0"
+
+  name        = "back_engineer_role"
+  stack       = "dev-qa-prod"
+  policy_arn  = [aws_iam_policy.eks_admin_policy.arn]
+}
+
+module "data-engineer-role" {
+  source  = "tf-mod/rbac-role/aws"
+  version = "1.0.0"
+
+  name        = "data_engineer_role"
+  stack       = "dev-qa-prod"
+  policy_arn  = ["arn:aws:iam::aws:policy/AmazonRedshiftFullAccess"]
+}
+
+module "site-reliability-engineer-role" {
+  source  = "tf-mod/rbac-role/aws"
+  version = "1.0.0"
+
+  name        = "site_reliability_engineer_role"
+  stack       = "dev-qa-prod"
+  policy_arn  = ["arn:aws:iam::aws:policy/AdministratorAccess", "arn:aws:iam::aws:policy/PowerUserAccess"]
+}
